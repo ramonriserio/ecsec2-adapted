03a9b599b0f2d4bd88551d5f2cbed488d84f1c38
Ramon Risério - DevOps Engineer <ramonriserio@hotmail.com>
2024-02-05 15:19:39 +0000

    escec2-adapted

---
+++ ec2.tf
@@ -0,0 +1,77 @@
+resource "aws_launch_template" "ecs_lt" {
+  name_prefix   = "ecs-template"
+  image_id      = "ami-0c45946ade6066f3d"
+  instance_type = "t2.micro"
+
+  key_name               = "humangov-ec2-key"
+  vpc_security_group_ids = [aws_security_group.security_group.id]
+  iam_instance_profile {
+    name = "ecsInstanceRole"
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "ecs-instance"
+    }
+  }
+
+  user_data = filebase64("${path.module}/ecs.sh")
+}
+
+resource "aws_autoscaling_group" "ecs_asg" {
+  vpc_zone_identifier = [aws_subnet.subnet.id, aws_subnet.subnet2.id]
+  desired_capacity    = 1
+  max_size            = 1
+  min_size            = 1
+
+  launch_template {
+    id      = aws_launch_template.ecs_lt.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "AmazonECSManaged"
+    value               = true
+    propagate_at_launch = true
+  }
+}
+
+//Alb
+resource "aws_lb" "ecs_alb" {
+  name               = "ecs-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.security_group.id]
+  subnets            = [aws_subnet.subnet.id, aws_subnet.subnet2.id]
+
+  tags = {
+    Name = "ecs-alb"
+  }
+}
+
+resource "aws_lb_listener" "ecs_alb_listener" {
+  load_balancer_arn = aws_lb.ecs_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_tg.arn
+  }
+}
+
+resource "aws_lb_target_group" "ecs_tg" {
+  name        = "ecs-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
+
+  health_check {
+    path = "/"
+  }
+}
+
+
+
